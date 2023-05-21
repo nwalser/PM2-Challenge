134411f766fc9ec43e4dd401db4289a4b79bd2b0
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-05-21 11:06:23 +0200

    updated cycle

--- PM2-Challenge/main.cpp
+++ PM2-Challenge/main.cpp
@@ -37,25 +37,24 @@ int main() {
   // create user button
   DigitalIn *user_button = new DigitalIn(BUTTON1);
 
-  // SERVO PC_8
-  // create front servo controller
+  // INSTANTIATE SERVOS
+  // SERVO FRONT
   Servo *servo_joint_front = new Servo(PC_8);
   Motion *motion_planner_front = new Motion();
-  motion_planner_front->setLimits(50, 30, 30);
+  motion_planner_front->setLimits(30, 30, 30);
   Map *angle_map_front = new Map(0, 175, 0.032, 0.120);
   ServoController *servo_controller_front = new ServoController(
       servo_joint_front, motion_planner_front, angle_map_front);
 
-  // SERVO PB_2
-  // create back servo controller
+  // SERVO BACK
   Servo *servo_joint_back = new Servo(PB_12);
   Motion *motion_planner_back = new Motion();
-  motion_planner_back->setLimits(50, 30, 30);
+  motion_planner_back->setLimits(30, 30, 30);
   Map *angle_map_back = new Map(0, 160, 0.033, 0.128);
   ServoController *servo_controller_back = new ServoController(
       servo_joint_back, motion_planner_back, angle_map_back);
 
-  // create motor objects
+  // INSTANTIATE MOTORS
   DigitalOut *enable_motors = new DigitalOut(PB_15);
   const float max_voltage = 12.0f;
   const float counts_per_turn = 20.0f * 78.125f;
@@ -63,10 +62,9 @@ int main() {
   // original k_gear value was 100, this was to low, don't know why :)
   const float k_gear = 120.0f / 78.125f;
   const float kp = 0.05f;
-  float max_speed_rps = 0.5f;
+  float max_speed_rps = 0.9f;
 
-  // m1
-  // create front motor controller
+  // FRONT MOTOR
   FastPWM pwm_front(PB_13);
   EncoderCounter encoder_front(PA_6, PC_7);
   PositionController *position_controller_front =
@@ -75,8 +73,7 @@ int main() {
   position_controller_front->setSpeedCntrlGain(kp * k_gear);
   position_controller_front->setMaxVelocityRPS(max_speed_rps);
 
-  // M2
-  // create back motor controller
+  // BACK MOTOR
   FastPWM pwm_back(PA_10);
   EncoderCounter encoder_back(PA_0, PA_1);
   PositionController *position_controller_back =
@@ -85,13 +82,13 @@ int main() {
   position_controller_back->setSpeedCntrlGain(kp * k_gear);
   position_controller_back->setMaxVelocityRPS(max_speed_rps);
 
-  // create robot object
+  // INSTANTIATE ROBOT
   Robot *robot = new Robot(servo_controller_front, servo_controller_back,
                            position_controller_front, position_controller_back,
                            enable_motors);
 #pragma endregion
 
-  // initialize all controlls
+  // initialize robot
   printf("Init \n");
   robot->init();
   WAIT_UNTIL_TRUE(robot->isIdle());
@@ -104,101 +101,60 @@ int main() {
   while (true) {
     robot->setJointAngles(0, 0);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    printf("On 0 \n");
 
-    ThisThread::sleep_for(4s);
+    robot->setJointAngles(0, 135);
+    WAIT_UNTIL_TRUE(robot->isIdle());
 
     robot->setJointAngles(90, 90);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    printf("On 90 \n");
-
-    ThisThread::sleep_for(4s);
-
-    robot->setJointAngles(160, 160);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    printf("On 160 \n");
-
-    ThisThread::sleep_for(4s);
-  }
-
-
-  while (true) {
-    robot->setJointAngles(0, 0);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
-
-    robot->setJointAngles(20, 125);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(4s);
-
-    robot->setJointAngles(90, 90);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
     // robot is flat on ground
-    robot->drive(150);
+    robot->drive(130);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
     robot->setJointAngles(90, 160, Robot::JointCorrectionMode::None);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    robot->drive(120);
+    robot->drive(80);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
     // robot is buckled up against the obstacle
-    robot->setJointAngles(120, 50, Robot::JointCorrectionMode::None);
+    robot->setJointAngles(110, 50, Robot::JointCorrectionMode::None);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
     
     robot->drive(100);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    robot->setJointAngles(70, 25, Robot::JointCorrectionMode::Back);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
-    
     // robot stands above the obstacle
-    robot->setJointAngles(15, 15, Robot::JointCorrectionMode::Back);
+    robot->setJointAngles(0, 0, Robot::JointCorrectionMode::Both);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    robot->drive(40);
+    robot->drive(10);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    robot->setJointAngles(55, 125);
+    robot->setJointAngles(0, 135);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    //robot->drive(80);
-    //WAIT_UNTIL_TRUE(robot->isIdle());
-    //ThisThread::sleep_for(50ms);
+    robot->setJointAngles(35, 135);
+    WAIT_UNTIL_TRUE(robot->isIdle());
+
+    robot->drive(90);
+    WAIT_UNTIL_TRUE(robot->isIdle());
 
     robot->setJointAngles(155, 90);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
-
-    robot->drive(100);
-    WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
     robot->setJointAngles(90, 90);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
-    robot->drive(190);
+    robot->drive(165);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(50ms);
 
 
     // stand up front
-    robot->setJointAngles(125, 0, Robot::JointCorrectionMode::Back);
+    robot->setJointAngles(130, 0, Robot::JointCorrectionMode::Back);
     WAIT_UNTIL_TRUE(robot->isIdle());
-    ThisThread::sleep_for(4s);
+    ThisThread::sleep_for(50ms);
 
     robot->setJointAngles(0, 0, Robot::JointCorrectionMode::Back);
     WAIT_UNTIL_TRUE(robot->isIdle());
